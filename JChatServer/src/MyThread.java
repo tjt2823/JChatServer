010420517047498377ad4195ba884243dbef23cb
Tom <Tom@192.168.2.12>
2016-09-27 20:30:16 -0400

    Made changes to user list handling in MyThread class

--- JChatServer/src/MyThread.java
+++ JChatServer/src/MyThread.java
@@ -5,66 +5,108 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 
-class MyThread implements Runnable {
+/**
+ * Thread class that is run by the server
+ * 
+ * @author Tom Thomas
+ */
+class MyThread implements Runnable
+{
 	Socket s;
-	ArrayList<Socket> al;
+	ArrayList<Socket> clients;
 	ArrayList<String> users;
 	String username;
 
-	MyThread(Socket s, ArrayList<Socket> al, ArrayList<String> users) {
+	/**
+	 * The thread is initialized when a client connects to the server which happens
+	 * when users log in. The thread then sends this message to all clients listening.
+	 * 
+	 * @param s
+	 * @param clients
+	 * @param users
+	 */
+	MyThread(Socket s, ArrayList<Socket> clients, ArrayList<String> users)
+	{
 		this.s = s;
-		this.al = al;
+		this.clients = clients;
 		this.users = users;
-		try {
-			DataInputStream dis = new DataInputStream(s.getInputStream());
-			username = dis.readUTF();
-			al.add(s);
+		
+		try
+		{
+			DataInputStream dIS = new DataInputStream(s.getInputStream());
+			username = dIS.readUTF();
+			clients.add(s);
 			users.add(username);
 			tellEveryOne("****** " + username + " Logged in at " + (new Date()) + " ******");
 			sendNewUserList();
-		} catch (Exception e) {
+		} catch (Exception e)
+		{
 			System.err.println("MyThread constructor  " + e);
 		}
 	}
 
-	public void run() {
+	/**
+	 * Runs on a loop listening for input messages and sends messages to
+	 * clients listening.
+	 */
+	public void run()
+	{
 		String s1;
-		try {
-			DataInputStream dis = new DataInputStream(s.getInputStream());
-			do {
-				s1 = dis.readUTF();
+		try
+		{
+			DataInputStream dIS = new DataInputStream(s.getInputStream());
+			do
+			{
+				s1 = dIS.readUTF();
+				
 				if (s1.toLowerCase().equals(JChatServer.LOGOUT_MESSAGE))
 					break;
+				
 				tellEveryOne(username + " said: " + " : " + s1);
 			} while (true);
-			DataOutputStream tdos = new DataOutputStream(s.getOutputStream());
-			tdos.writeUTF(JChatServer.LOGOUT_MESSAGE);
-			tdos.flush();
+			
+			DataOutputStream dOS = new DataOutputStream(s.getOutputStream());
+			dOS.writeUTF(JChatServer.LOGOUT_MESSAGE);
+			dOS.flush();
+			
 			users.remove(username);
 			tellEveryOne("****** " + username + " Logged out at " + (new Date()) + " ******");
 			sendNewUserList();
-			al.remove(s);
+			clients.remove(s);
 			s.close();
 
-		} catch (Exception e) {
+		} catch (Exception e)
+		{
 			System.out.println("MyThread Run" + e);
 		}
 	}
 
-	public void sendNewUserList() {
+	/**
+	 * Called when a user logs out.
+	 */
+	public void sendNewUserList()
+	{
 		tellEveryOne(JChatServer.UPDATE_USERS + users.toString());
-
 	}
 
-	public void tellEveryOne(String s1) {
-		Iterator<Socket> i = al.iterator();
-		while (i.hasNext()) {
-			try {
+	/**
+	 * Sends messages to all clients
+	 * 
+	 * @param s1 The message to send
+	 */
+	public void tellEveryOne(String s1)
+	{
+		Iterator<Socket> i = clients.iterator();
+		while (i.hasNext())
+		{
+			try
+			{
 				Socket temp = (Socket) i.next();
-				DataOutputStream dos = new DataOutputStream(temp.getOutputStream());
-				dos.writeUTF(s1);
-				dos.flush();
-			} catch (Exception e) {
+				DataOutputStream dOS = new DataOutputStream(temp.getOutputStream());
+				dOS.writeUTF(s1);
+				dOS.flush();
+			} catch (Exception e)
+			{
 				System.err.println("TellEveryOne " + e);
 			}
 		}
